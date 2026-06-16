c100ca26f4de313b8baf43328007d6d1c43e3cb6
Zulfiya Saiyed <zulfiya@PepWear.com>
2026-06-16 11:03:25 -0500

    feat: frontend vite scaffold with tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#002856",
+        brand: "#d35e13",
+      },
+    },
+  },
+  plugins: [],
+}
